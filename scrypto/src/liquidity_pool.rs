e59e696d1291eb01bb6b47369acdd02a0116c02a
davidcuellarrdx <david.cuellar@rdx.works>
2023-12-22 16:24:42 +0000

    Feat: Prettier

--- scrypto/src/liquidity_pool.rs
+++ scrypto/src/liquidity_pool.rs
@@ -3,7 +3,6 @@ use crate::utils::*;
 
 #[blueprint]
 mod liquidity_pool {
-
     enable_method_auth! {
         roles {
             withdrawer => updatable_by: [OWNER, SELF];
@@ -25,32 +24,42 @@ mod liquidity_pool {
     }
 
     impl LiquidityPool {
-
         pub fn instantiate_liquidity_pool(
             bucket_a: FungibleBucket,
             bucket_b: FungibleBucket,
-            fee: Decimal,
-        ) -> (Global<LiquidityPool>, FungibleBucket){
-
-            
-            assert!(!bucket_a.is_empty() && !bucket_b.is_empty(), "You must pass in an initial supply of each token");
+            fee: Decimal
+        ) -> (Global<LiquidityPool>, FungibleBucket) {
+            assert!(
+                !bucket_a.is_empty() && !bucket_b.is_empty(),
+                "You must pass in an initial supply of each token"
+            );
 
             assert!(fee >= dec!(0) && fee <= dec!(1), "Fee must be between 0 and 1");
 
-            let (address_reservation, component_address) = 
-                Runtime::allocate_component_address(LiquidityPool::blueprint_id());
+            let (address_reservation, component_address) = Runtime::allocate_component_address(
+                LiquidityPool::blueprint_id()
+            );
 
             // Sorting the buckets and then creating the hashmap of the vaults from the sorted buckets
-            let (bucket_a, bucket_b): (FungibleBucket, FungibleBucket) = sort_buckets(bucket_a, bucket_b);
-            let addresses: (ResourceAddress, ResourceAddress) = (bucket_a.resource_address(), bucket_b.resource_address());
+            let (bucket_a, bucket_b): (FungibleBucket, FungibleBucket) = sort_buckets(
+                bucket_a,
+                bucket_b
+            );
+            let addresses: (ResourceAddress, ResourceAddress) = (
+                bucket_a.resource_address(),
+                bucket_b.resource_address(),
+            );
 
             let lp_id: String = format!("{:?}-{:?}", addresses.0, addresses.1);
 
             let pair_name: String = address_pair_symbol(addresses.0, addresses.1);
 
             info!(
-                "[Pool Creation]: Creating new pool between tokens: {}, of name: {}, Ratio: {}:{}", 
-                lp_id, pair_name, bucket_a.amount(), bucket_b.amount()
+                "[Pool Creation]: Creating new pool between tokens: {}, of name: {}, Ratio: {}:{}",
+                lp_id,
+                pair_name,
+                bucket_a.amount(),
+                bucket_b.amount()
             );
 
             let mut vaults: HashMap<ResourceAddress, FungibleVault> = HashMap::new();
@@ -58,37 +67,43 @@ mod liquidity_pool {
             vaults.insert(bucket_b.resource_address(), FungibleVault::with_bucket(bucket_b));
 
             let pool_units = ResourceBuilder::new_fungible(OwnerRole::None)
-                .metadata(metadata!(
+                .metadata(
+                    metadata!(
                     init {
                         "name" => format!("{} LP Tracking Token", pair_name), locked;
                         "symbol" => format!("LP-{}", pair_name), locked;
                         "lp_id" => format!("{}", lp_id), locked;
                     }
-                ))
-                .mint_roles(mint_roles!(
+                )
+                )
+                .mint_roles(
+                    mint_roles!(
                     minter => rule!(require(global_caller(component_address)));
                     minter_updater => rule!(deny_all);
-                ))
-                .burn_roles(burn_roles!(
+                )
+                )
+                .burn_roles(
+                    burn_roles!(
                     burner => rule!(require(global_caller(component_address)));
                     burner_updater => rule!(deny_all);
-                ))
+                )
+                )
                 .mint_initial_supply(100);
 
-            
-
-            let liquidity_pool = Self{
+            let liquidity_pool = (Self {
                 vaults: vaults,
                 pool_units_resource_manager: pool_units.resource_manager(),
                 fee: fee,
-            }
-            .instantiate()
-            .prepare_to_globalize(OwnerRole::None)
-            .roles(roles!( 
+            })
+                .instantiate()
+                .prepare_to_globalize(OwnerRole::None)
+                .roles(
+                    roles!( 
                 withdrawer => rule!(require(global_caller(component_address)));
-            ))
-            .with_address(address_reservation)
-            .globalize();
+            )
+                )
+                .with_address(address_reservation)
+                .globalize();
 
             (liquidity_pool, pool_units)
         }
@@ -97,119 +112,115 @@ mod liquidity_pool {
             return self.vaults.keys().cloned().collect::<Vec<ResourceAddress>>();
         }
 
-        pub fn other_resource_address(
-            &self,
-            resource_address: ResourceAddress
-        ) -> ResourceAddress {
+        pub fn other_resource_address(&self, resource_address: ResourceAddress) -> ResourceAddress {
             let addresses: Vec<ResourceAddress> = self.addresses();
-            return if addresses[0] == resource_address {addresses[1]} else {addresses[0]};
+            return if addresses[0] == resource_address { addresses[1] } else { addresses[0] };
         }
 
-        pub fn swap(&mut self, input_tokens: FungibleBucket) -> FungibleBucket{
+        pub fn swap(&mut self, input_tokens: FungibleBucket) -> FungibleBucket {
+            let output_resource_address = self.other_resource_address(
+                input_tokens.resource_address()
+            );
 
-            let output_resource_address = self.other_resource_address(input_tokens.resource_address());
+            let input_tokens_vault_amount = self.vaults[&input_tokens.resource_address()].amount();
+            let output_tokens_vault_amount = self.vaults[&output_resource_address].amount();
 
-            let input_tokens_vault_amount =self.vaults[&input_tokens.resource_address()].amount();
-            let output_tokens_vault_amount =self.vaults[&output_resource_address].amount();
-            
-
-            // Calculate the output amount of tokens based on the input amount 
+            // Calculate the output amount of tokens based on the input amount
             // and the pool fees
-            let output_amount: Decimal = (output_tokens_vault_amount
-            * (dec!("1") - self.fee)
-            * input_tokens.amount())
-            / (input_tokens_vault_amount + input_tokens.amount() 
-            * (dec!("1") - self.fee));
+            let output_amount: Decimal =
+                (output_tokens_vault_amount * (dec!("1") - self.fee) * input_tokens.amount()) /
+                (input_tokens_vault_amount + input_tokens.amount() * (dec!("1") - self.fee));
 
-            
             // Perform the swapping operation
             self.vaults.get_mut(&input_tokens.resource_address()).unwrap().put(input_tokens);
 
             self.vaults.get_mut(&output_resource_address).unwrap().take(output_amount)
-
-            
         }
-        
+
         pub fn add_liquidity(
             &mut self,
             token1: FungibleBucket,
-            token2: FungibleBucket,
+            token2: FungibleBucket
         ) -> (FungibleBucket, FungibleBucket, FungibleBucket) {
-
-        
             // Getting the values of `dm` and `dn` based on the sorted buckets
 
-            let (mut bucket_a, mut bucket_b): (FungibleBucket, FungibleBucket) = sort_buckets(token1, token2);
+            let (mut bucket_a, mut bucket_b): (FungibleBucket, FungibleBucket) = sort_buckets(
+                token1,
+                token2
+            );
 
             let dm: Decimal = bucket_a.amount();
             let dn: Decimal = bucket_b.amount();
-        
+
             // Getting the values of m and n from the liquidity pool vaults
             let m: Decimal = self.vaults[&bucket_a.resource_address()].amount();
             let n: Decimal = self.vaults[&bucket_b.resource_address()].amount();
-        
-            // Calculate the amount of tokens which will be added to each one of 
+
+            // Calculate the amount of tokens which will be added to each one of
             //the vaults
-            let (amount_a, amount_b): (Decimal, Decimal) =
-                if ((m == Decimal::zero()) | (n == Decimal::zero())) 
-                    | ((m / n) == (dm / dn)) 
-                {
-                    // Case 1
-                    (dm, dn)
-                } else if (m / n) < (dm / dn) {
-                    // Case 2
-                    (dn * m / n, dn)
-                } else {
-                    // Case 3
-                    (dm, dm * n / m)
-                };
-        
+            let (amount_a, amount_b): (Decimal, Decimal) = if
+                (m == Decimal::zero()) |
+                (n == Decimal::zero()) |
+                (m / n == dm / dn)
+            {
+                // Case 1
+                (dm, dn)
+            } else if m / n < dm / dn {
+                // Case 2
+                ((dn * m) / n, dn)
+            } else {
+                // Case 3
+                (dm, (dm * n) / m)
+            };
+
             // Depositing the amount of tokens calculated into the liquidity pool
-        
+
             self.vaults.get_mut(&bucket_a.resource_address()).unwrap().put(bucket_a.take(amount_a));
             self.vaults.get_mut(&bucket_b.resource_address()).unwrap().put(bucket_b.take(amount_b));
 
-        
             // Mint pool units tokens to the liquidity provider
-            let pool_units_amount: Decimal =
-                if self.pool_units_resource_manager.total_supply().unwrap() == Decimal::zero() {
-                    dec!("100.00")
-                } else {
-                    amount_a * self.pool_units_resource_manager.total_supply().unwrap() / m
-                };
-            let pool_units: FungibleBucket = self.pool_units_resource_manager.mint(pool_units_amount).as_fungible();
-        
-            // Return the remaining tokens to the caller as well as the pool units 
+            let pool_units_amount: Decimal = if
+                self.pool_units_resource_manager.total_supply().unwrap() == Decimal::zero()
+            {
+                dec!("100.00")
+            } else {
+                (amount_a * self.pool_units_resource_manager.total_supply().unwrap()) / m
+            };
+            let pool_units: FungibleBucket = self.pool_units_resource_manager
+                .mint(pool_units_amount)
+                .as_fungible();
+
+            // Return the remaining tokens to the caller as well as the pool units
             // tokens
             (bucket_a, bucket_b, pool_units)
         }
 
-        pub fn remove_liquidity_logic(&mut self, 
-            address1: ResourceAddress, 
-            address2: ResourceAddress, 
-            bucket1_amount: Decimal, 
-            bucket2_amount: Decimal ) -> (FungibleBucket, FungibleBucket)
-        {
-            
-                    
+        pub fn remove_liquidity_logic(
+            &mut self,
+            address1: ResourceAddress,
+            address2: ResourceAddress,
+            bucket1_amount: Decimal,
+            bucket2_amount: Decimal
+        ) -> (FungibleBucket, FungibleBucket) {
             (
-            self.vaults.get_mut(&address1).unwrap().take(bucket1_amount),
-            self.vaults.get_mut(&address2).unwrap().take(bucket2_amount)
+                self.vaults.get_mut(&address1).unwrap().take(bucket1_amount),
+                self.vaults.get_mut(&address2).unwrap().take(bucket2_amount),
             )
         }
 
-        pub fn remove_liquidity(&mut self, pool_units: FungibleBucket) -> (FungibleBucket, FungibleBucket) {
-    
+        pub fn remove_liquidity(
+            &mut self,
+            pool_units: FungibleBucket
+        ) -> (FungibleBucket, FungibleBucket) {
             assert!(
                 pool_units.resource_address() == self.pool_units_resource_manager.address(),
                 "Wrong token type passed in"
             );
-        
+
             // Calculate the share based on the input LP tokens.
-            let share = pool_units.amount() / 
-                self.pool_units_resource_manager.total_supply().unwrap();
-        
-        
+            let share =
+                pool_units.amount() / self.pool_units_resource_manager.total_supply().unwrap();
+
             // Burn the LP tokens received
             pool_units.burn();
 
@@ -218,7 +229,7 @@ mod liquidity_pool {
 
             let bucket1_amount = self.vaults.get_mut(&addresses[0]).unwrap().amount() * share;
             let bucket2_amount = self.vaults.get_mut(&addresses[1]).unwrap().amount() * share;
-        
+
             // Return the withdrawn tokens
             // (
             //     self.vaults.get_mut(&addresses[0]).unwrap().take(bucket1_amount),
@@ -226,6 +237,5 @@ mod liquidity_pool {
             // )
             self.remove_liquidity_logic(addresses[0], addresses[1], bucket1_amount, bucket2_amount)
         }
-
     }
 }
--- scrypto/src/scrypto_dex.rs
+++ scrypto/src/scrypto_dex.rs
@@ -5,32 +5,30 @@ use crate::liquidity_pool::liquidity_pool::LiquidityPoolFunctions;
 
 #[blueprint]
 mod scryptodex_module {
-    
     struct ScryptoDex {
         liquidity_pools: HashMap<(ResourceAddress, ResourceAddress), Global<LiquidityPool>>,
-        tracking_token_address_pair_mapping: HashMap<ResourceAddress, (ResourceAddress, ResourceAddress)>,
+        tracking_token_address_pair_mapping: HashMap<
+            ResourceAddress,
+            (ResourceAddress, ResourceAddress)
+        >,
     }
 
     impl ScryptoDex {
-        
         pub fn instantiate_scrypto_dex() -> Global<ScryptoDex> {
-
-            Self {
-                liquidity_pools: HashMap::new(), 
-                tracking_token_address_pair_mapping: HashMap::new()
-            }
-            .instantiate()
-            .prepare_to_globalize(OwnerRole::None)
-            .globalize()
+            (Self {
+                liquidity_pools: HashMap::new(),
+                tracking_token_address_pair_mapping: HashMap::new(),
+            })
+                .instantiate()
+                .prepare_to_globalize(OwnerRole::None)
+                .globalize()
         }
 
-        pub fn pool_exists(
-            &self,
-            address1: ResourceAddress,
-            address2: ResourceAddress
-        ) -> bool {
-
-            let sorted_addresses: (ResourceAddress, ResourceAddress) = sort_addresses(address1, address2);
+        pub fn pool_exists(&self, address1: ResourceAddress, address2: ResourceAddress) -> bool {
+            let sorted_addresses: (ResourceAddress, ResourceAddress) = sort_addresses(
+                address1,
+                address2
+            );
             return self.liquidity_pools.contains_key(&sorted_addresses);
         }
 
@@ -41,8 +39,8 @@ mod scryptodex_module {
             label: String
         ) {
             assert!(
-                self.pool_exists(address1, address2), 
-                "[{}]: No liquidity pool exists for the given address pair.", 
+                self.pool_exists(address1, address2),
+                "[{}]: No liquidity pool exists for the given address pair.",
                 label
             );
         }
@@ -54,8 +52,8 @@ mod scryptodex_module {
             label: String
         ) {
             assert!(
-                !self.pool_exists(address1, address2), 
-                "[{}]: A liquidity pool with the given address pair already exists.", 
+                !self.pool_exists(address1, address2),
+                "[{}]: A liquidity pool with the given address pair already exists.",
                 label
             );
         }
@@ -63,29 +61,34 @@ mod scryptodex_module {
         pub fn new_liquidity_pool(
             &mut self,
             token1: FungibleBucket,
-            token2: FungibleBucket,
+            token2: FungibleBucket
         ) -> FungibleBucket {
             // Checking if a liquidity pool already exists between these two tokens
             self.assert_pool_doesnt_exists(
-                token1.resource_address(), token2.resource_address(), 
+                token1.resource_address(),
+                token2.resource_address(),
                 String::from("New Liquidity Pool")
             );
 
             // Sorting the two buckets according to their resource addresses and creating a liquidity pool from these
             // two buckets.
             let (bucket1, bucket2): (FungibleBucket, FungibleBucket) = sort_buckets(token1, token2);
-            let addresses: (ResourceAddress, ResourceAddress) = (bucket1.resource_address(), bucket2.resource_address()); 
-            
-            let (liquidity_pool, tracking_tokens) = 
-                Blueprint::<LiquidityPool>::instantiate_liquidity_pool(bucket1, bucket2, dec!("0.3"));
-
-            // Adding the liquidity pool to the hashmap of all liquidity pools
-            self.liquidity_pools.insert(
-                addresses,
-                liquidity_pool
+            let addresses: (ResourceAddress, ResourceAddress) = (
+                bucket1.resource_address(),
+                bucket2.resource_address(),
             );
 
-            // Adding the resource address of the tracking tokens to the hashmap that maps the tracking tokens with 
+            let (liquidity_pool, tracking_tokens) =
+                Blueprint::<LiquidityPool>::instantiate_liquidity_pool(
+                    bucket1,
+                    bucket2,
+                    dec!("0.3")
+                );
+
+            // Adding the liquidity pool to the hashmap of all liquidity pools
+            self.liquidity_pools.insert(addresses, liquidity_pool);
+
+            // Adding the resource address of the tracking tokens to the hashmap that maps the tracking tokens with
             // the address of their token pairs
             self.tracking_token_address_pair_mapping.insert(
                 tracking_tokens.resource_address(),
@@ -99,22 +102,38 @@ mod scryptodex_module {
         pub fn add_liquidity(
             &mut self,
             token1: FungibleBucket,
-            token2: FungibleBucket,
+            token2: FungibleBucket
         ) -> (Option<FungibleBucket>, Option<FungibleBucket>, FungibleBucket) {
             // Sorting the two buckets of tokens passed to this method and getting the addresses of their resources.
             let (bucket1, bucket2): (FungibleBucket, FungibleBucket) = sort_buckets(token1, token2);
-            let addresses: (ResourceAddress, ResourceAddress) = (bucket1.resource_address(), bucket2.resource_address()); 
+            let addresses: (ResourceAddress, ResourceAddress) = (
+                bucket1.resource_address(),
+                bucket2.resource_address(),
+            );
 
             // Attempting to get the liquidity pool component associated with the provided address pair.
-            let optional_liquidity_pool: Option<&Global::<LiquidityPool>> = self.liquidity_pools.get(&addresses);
+            let optional_liquidity_pool: Option<&Global<LiquidityPool>> = self.liquidity_pools.get(
+                &addresses
+            );
             match optional_liquidity_pool {
-                Some (liquidity_pool) => { // If it matches it means that the liquidity pool exists.
-                    info!("[DEX Add Liquidity]: Pool for {:?} already exists. Adding liquidity directly.", addresses);
-                    let returns: (FungibleBucket, FungibleBucket, FungibleBucket) = liquidity_pool.add_liquidity(bucket1, bucket2);
+                Some(liquidity_pool) => {
+                    // If it matches it means that the liquidity pool exists.
+                    info!(
+                        "[DEX Add Liquidity]: Pool for {:?} already exists. Adding liquidity directly.",
+                        addresses
+                    );
+                    let returns: (
+                        FungibleBucket,
+                        FungibleBucket,
+                        FungibleBucket,
+                    ) = liquidity_pool.add_liquidity(bucket1, bucket2);
                     (Some(returns.0), Some(returns.1), returns.2)
                 }
-                None => { 
-                    info!("[DEX Add Liquidity]: Pool for {:?} doesn't exist. Creating a new one.", addresses);
+                None => {
+                    info!(
+                        "[DEX Add Liquidity]: Pool for {:?} doesn't exist. Creating a new one.",
+                        addresses
+                    );
                     (None, None, self.new_liquidity_pool(bucket1, bucket2))
                 }
             }
@@ -127,13 +146,23 @@ mod scryptodex_module {
             // Check to make sure that the tracking tokens provided are indeed valid tracking tokens that belong to this
             // DEX.
             assert!(
-                self.tracking_token_address_pair_mapping.contains_key(&tracking_tokens.resource_address()),
+                self.tracking_token_address_pair_mapping.contains_key(
+                    &tracking_tokens.resource_address()
+                ),
                 "[DEX Remove Liquidity]: The tracking tokens given do not belong to this exchange."
+            );
+
+            info!(
+                "[DEX Remove Liquidity]: Pool for {:?}. Remove liquidity directly.",
+                tracking_tokens
             );
 
             // Getting the address pair associated with the resource address of the tracking tokens and then requesting
             // the removal of liquidity from the liquidity pool
-            let addresses: (ResourceAddress, ResourceAddress) = self.tracking_token_address_pair_mapping[&tracking_tokens.resource_address()];
+            let addresses: (
+                ResourceAddress,
+                ResourceAddress,
+            ) = self.tracking_token_address_pair_mapping[&tracking_tokens.resource_address()];
             return self.liquidity_pools[&addresses].remove_liquidity(tracking_tokens);
         }
 
@@ -143,17 +172,18 @@ mod scryptodex_module {
             output_resource_address: ResourceAddress
         ) -> FungibleBucket {
             // Checking if there does exist a liquidity pool for the given pair of tokens
-            self.assert_pool_exists(tokens.resource_address(), output_resource_address, String::from("DEX Swap"));
+            self.assert_pool_exists(
+                tokens.resource_address(),
+                output_resource_address,
+                String::from("DEX Swap")
+            );
 
             // Sorting the two addresses passed, getting the associated liquidity pool and then performing the swap.
             let sorted_addresses: (ResourceAddress, ResourceAddress) = sort_addresses(
-                tokens.resource_address(), 
+                tokens.resource_address(),
                 output_resource_address
             );
             return self.liquidity_pools[&sorted_addresses].swap(tokens);
         }
-
-        
-
     }
 }
